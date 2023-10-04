d68b25d14f4af8378e3b959360eeb9231a84b88e
Martin <martin999b@gmail.com>
2023-10-04 10:38:04 +0200

    Update cors headers for render

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -4,7 +4,8 @@ import { NextResponse } from "next/server";
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Headers":
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
 };
 
 export async function GET(request: Request) {
